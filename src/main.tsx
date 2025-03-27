f5d867ab15bec1a97a40a71f68c47b1522e4e57e
NicoHC185 <nikohidalgo.castillo@gmail.com>
2025-03-27 00:46:34 -0300

    :mute: clear clg

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import mapboxgl from "mapbox-gl"; // or "const mapboxgl = require('mapbox-gl');"
 
 mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
-console.log(import.meta.env.VITE_MAPBOX_TOKEN);
-
 if (!navigator.geolocation) {
   alert("Tu navegador no tiene opción de geolocation");
   throw new Error("Tu navegador no tiene opción de geolocation");
